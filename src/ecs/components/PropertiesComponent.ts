35821188d89ee7248bcb52325533aa824d1124f4
sinkapoy <sharshov-ivan@yandex.ru>
2023-07-09 20:21:51 +0300

    refactoring

--- src/ecs/components/PropertiesComponent.ts
+++ src/ecs/components/PropertiesComponent.ts
@@ -1,10 +1,11 @@
 export enum PropertyAccessMode {
     none = 0,
-    read = 0b001,
-    write = 0b010,
-    rw = 0b011,
-    notify = 0b100,
-    rwn = 0b111,
+    read = 0b0001,
+    write = 0b0010,
+    rw = 0b0011,
+    notify = 0b0100,
+    rwn = 0b0111,
+    rn = 0b0101,
 }
 
 export interface IProperty {
@@ -16,22 +17,22 @@ export interface IProperty {
     description?: string;
 }
 
-export class Property<T extends string | number | boolean = string> {
+export class Property<T extends string | number | boolean = string | number | boolean> {
     description?: string;
-    
+
     constructor(
         public readonly id: string,
         public readonly accessMode: PropertyAccessMode | number,
         public value: T | null = null,
-        min?: number,
-        max?: number,
+        public min?: number,
+        public max?: number,
     ) {
     }
 }
 
 export class PropertiesComponent extends Map<string, Property<any>> {
-    createPropertyFromJson(json: IProperty){
-        if(!json.id || !json.accessMode){
+    createPropertyFromJson(json: IProperty) {
+        if (!json.id || !json.accessMode) {
             throw new Error('try convert broken json to gadget property\n' + JSON.stringify(json));
         }
         this.set(json.id, new Property(
@@ -43,7 +44,7 @@ export class PropertiesComponent extends Map<string, Property<any>> {
         ));
     }
 
-    getTyped<T extends string | number | boolean>(string){
+    getTyped<T extends string | number | boolean>(string) {
         return this.get(string) as Property<T> | undefined;
     }
 }
