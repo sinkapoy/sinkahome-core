0f24692b508fb096fddc6fd814c6e8df985069f5
sinkapoy <sharshov-ivan@yandex.ru>
2023-07-09 20:21:39 +0300

    add create gadget function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,9 @@
-import { Node, NodeList } from "@ash.ts/ash";
+import { Entity, Node, NodeList } from "@ash.ts/ash";
+import type { uuidT } from "../exportedTypes/common";
+import { PropertiesComponent } from "../ecs/components/PropertiesComponent";
+import { ActionsComponent } from "../ecs/components/ActionsComponent";
+import { EventsComponent } from "../ecs/components/EventsComponent";
+import { GadgetComponent } from "../ecs/components/GadgetComponent";
 
 export function foreachNode<T extends Node>(list: NodeList<T>, cb: (node: T) => void) {
     let node = list.head;
@@ -6,4 +11,15 @@ export function foreachNode<T extends Node>(list: NodeList<T>, cb: (node: T) => 
         cb(node);
         node = node.next;
     }
+}
+
+export function createGadget(uuid: uuidT, own: boolean) {
+    const entity = new Entity(uuid);
+    entity
+        .add(new GadgetComponent(uuid, own))
+        .add(new PropertiesComponent())
+        .add(new ActionsComponent())
+        .add(new EventsComponent());
+
+    return entity;
 }
