661858dff8233568a8c75ba2aefae8c2ba7ab625
sinkapoy <sharshov-ivan@yandex.ru>
2023-07-23 12:58:54 +0300

    fix browser build

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { HomeSystem } from "@root/src/ecs/HomeSystem";
 import { HomeEngine } from "@root/src/ecs/HomeEngine";
 export * from "@root/src/ecs/components/export";
 export * from "@root/src/ecs/nodes/common";
+export * from "@root/src/ecs/nodes/common";
 export * from "@root/src/utils/ArrayMap";
 export * from "@root/src/utils/common";
 
