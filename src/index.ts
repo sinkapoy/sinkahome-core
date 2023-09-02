8b45ecbca3d79d7e8674bcc899deaa32a989db8a
sinkapoy <sharshov-ivan@yandex.ru>
2023-09-02 13:50:32 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
 
 export { HomeSystem } from "@root/src/ecs/HomeSystem";
 import { HomeEngine } from "@root/src/ecs/HomeEngine";
+import { IHomeCoreEvents } from "./exportedTypes/common";
 export * from "@root/src/ecs/components/export";
 export * from "@root/src/ecs/nodes/common";
 export * from "@root/src/ecs/nodes/common";
 export * from "@root/src/utils/ArrayMap";
 export * from "@root/src/utils/common";
 
-export const homeEngine = new HomeEngine<Record<string, any[]>>();
+export const homeEngine = new HomeEngine<Record<string, any[]> & IHomeCoreEvents>();
 export type HomeEngineT<T> = HomeEngine<T>;
